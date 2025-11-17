aef7e68f26b2e0bd9e556f7a40b6fc8119b97d9c
François-Xavier <fx.feck08@gmail.com>
2025-11-17 10:26:56 +0100

    fix: ajout des liens React Router pour Mentions légales et Contact dans le footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,18 @@
+import { NavLink } from "react-router-dom";
+
 function Footer() {
   return (
     <footer>
       <p>© 2025 Trouve ton artisan</p>
       <p>101 cours Charlemagne, 69269 LYON CEDEX 02 — +33 (0)4 26 73 40 00</p>
       <p>
-        <a href="#">Mentions légales</a> | <a href="#">Contact</a>
+        <NavLink to="/mentions-legales" className="text-decoration-none">
+          Mentions légales
+        </NavLink>{" "}
+        |{" "}
+        <NavLink to="/contact" className="text-decoration-none">
+          Contact
+        </NavLink>
       </p>
     </footer>
   );
